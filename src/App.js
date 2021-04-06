1a2bdf631f5d4127240b87be882bf3bb228723bf
陳詩蕾 <shihlei@chenshileide-MacBook-Pro.local>
2021-04-06 23:12:54 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+const initialState = {
+  input:'',
+  imageUrl:'',//url應該要在點擊submit後顯示
+  box: {}, //include values we receive
+  route: 'signin',//從signin開始
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0, //記錄分數
+    joined: '' //自動建立日期 
+  }
+}
+
+class App extends Component {
+  constructor(){//宣告state
+    super(); //呼叫parent 的constructor，很重要！！
+    this.state = initialState;//state為在App中改變的內容
+  }
+
+  loadUser = (data) => {
+    //update user that we receive
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries, 
+      joined: data.joined 
+    }})
+  }
+
+  //這個function會return object
+  claculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  //receive claculateFaceLocation's return value
+  displayFaceBox = (box) =>{
+    //state(box) set with box
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) =>{
+    //update input component
+    this.setState({input : event.target.value});//拿value
+  }
+
+  onButtonSubmit = () =>{
+    //當imageUrl拿到input後會update
+    //pass imageUrl to FaceRecognition
+    this.setState({imageUrl:this.state.input});
+      fetch('https://lit-depths-96413.herokuapp.com/imageurl', {//用來在前端隱藏api key，移到後端做
+        method: 'post',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input
+        })
+      })
+      .then(response => response.json())//因為是fetch，所以要做response.json()
+      .then(response => {
+        if(response){
+          fetch('https://lit-depths-96413.herokuapp.com/image', {
+            method: 'put',
+            //header is object
+            headers: {'Content-type': 'application/json'},
+            //object所以要包json
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              //update count
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log)//error handling
+        }
+        this.displayFaceBox(this.claculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  //當有人signin之後進入home頁面
+  onRouteChange = (route) => {
+    if(route ==='signout'){
+      this.setState(initialState)
+    }
+    else if(route ==='home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render(){
+    const {isSignedIn, route, box, imageUrl} = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+            params={particlesOptions}
+          />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route ==='home' 
+          //click之後，route會變成signin
+          //html用{}包起來就會變成js格式
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
+              <ImageLinkForm 
+                onInputChange = {this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          :(
+            this.state.route === 'signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            ) 
+        } 
+      </div> 
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+class Signin extends React.Component {
+	//creat state
+	constructor(props){
+		super(props);
+		this.state = {
+			signInEmail: '',
+			signInPassword: ''
+		}
+	}
+
+	onEmailChange = (event) => {
+		this.setState({signInEmail: event.target.value})
+	}
+
+	onPasswordChange = (event) => {
+		this.setState({signInPassword: event.target.value})
+	}
+
+	//event, use state to fetch
+	onSubmitSignIn = () => {
+		//send info to server
+		fetch('https://lit-depths-96413.herokuapp.com/signin', {
+			method: 'post',
+			//header is object
+			headers: {'Content-type': 'application/json'},
+			//object所以要包json
+			body: JSON.stringify({
+				email: this.state.signInEmail,
+				password: this.state.signInPassword
+			})
+		})
+		.then(response => response.json())
+		.then(user => {
+			//see if user.id exists and call two functions if a user is returned from the backend
+			if(user.id){
+				//load user
+				this.props.loadUser(user);
+				//change route
+				this.props.onRouteChange('home');
+			}
+		})
+	}
+
+	render(){
+		const { onRouteChange } = this.props;
+		return (
+			<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+				<main className="pa4 black-80">
+				  <div className="measure">
+				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+				      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+				      <div className="mt3">
+				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+				        <input 
+				        	className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+				        	type="email" 
+				        	name="email-address"  
+				        	id="email-address" 
+				        	onChange = {this.onEmailChange}
+				        />
+				      </div>
+				      <div className="mv3">
+				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+				        <input 
+				        	className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+				        	type="password" 
+				        	name="password"  
+				        	id="password" 
+				        	onChange = {this.onPasswordChange}
+				        />
+				      </div>
+				    </fieldset>
+				    <div className="">
+				      <input
+				      	//定義function
+				      	//onClick後才會呼叫function
+				      	onClick={this.onSubmitSignIn}
+				      	className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+				      	type="submit" 
+				      	value="SignIn" 
+				      />
+				    </div>
+				    <div className="lh-copy mt3">
+				      <p onClick={() => onRouteChange('register')} href="#0" className="f6 link dim black db pointer">Register</p>
+				    </div>
+				  </div>
+				</main>
+			</article>
+		);
+	}
+}
+
+export default Signin;
